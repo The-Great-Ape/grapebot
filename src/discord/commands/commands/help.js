cc8f35f5de2ac7330fad2eb2e573a6c0392b0f80
Alex Babalitis <alexandros.babalitis@gmail.com>
2021-05-19 15:16:50 +0100

    Can check the balance for a given account!

--- src/discord/commands/commands/help.js
+++ src/discord/commands/commands/help.js
@@ -12,16 +12,11 @@ export default {
 
     const embed = new Discord.MessageEmbed();
     embed
-      .setColor('#0099ff')
-      .setTitle('Solbot')
-      .setDescription('Hi! I\'m Solbot. 😁\nYou can use me to send and receive SOL through discord!\n🚨 Warning 🚨 This wallet'
-          + ' is not built to be the most secure wallet ever and provides no guarantees for 100% bug-free code and takes no responsibility for the loss'
-          + ' of funds. Because of the way discord bots work, we need to store your private keys server side while you\'re logged in\n'
-          + 'Please use the bot to create a new wallet to be used in discord and only transfer small amounts to it.\n'
-          + 'ℹ️ To keep your wallet info safe, only the "!send" command is available in a public discord channel. DM the bot for all other commands. ℹ️️'
-          + '\nIf you\'re interested in the source code, go here: https://github.com/paul-schaaf/solbot')
+      .setColor('#2E145D')
+      .setTitle('Grapebot')
+      .setDescription('Hi! I\'m Grapebot. This is my helper description!')
       .addFields(...commandInstructions)
-      .setFooter('🚨 Repeated Warning 🚨 We have to save your private keys server side while you\'re logged in. Do not use your real wallet for this bot.');
+      .setFooter('');
 
     message.channel.send(embed);
   },
